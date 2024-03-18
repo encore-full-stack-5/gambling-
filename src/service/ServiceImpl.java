d4b1d9adb3e504b55dd8d78786e36569b498d1bc
giraffenostrils <dreamthre@gamil.com>
2024-03-18 16:30:35 +0900

    #29

--- src/service/ServiceImpl.java
+++ src/service/ServiceImpl.java
@@ -386,3 +386,55 @@ public class ServiceImpl implements Service {
         return null;
     }
 }
+
+    List<String> drawCard = new ArrayList<>();
+    List<String> pickMyCardResult = new ArrayList<>();
+    List<String> pickComputerCardResult = new ArrayList<>();
+    List<Integer> resultNumList = new ArrayList<>();
+    List<String> resultStringList = new ArrayList<>();
+    // 카드 만들기
+    @Override
+    public List<Card> makeCard() {
+        List<Card> deck = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            if (i == 2 || i == 5 || i == 6) {
+                Card card = new Card(i, false);
+                deck.add(card);
+            } else {
+                Card card = new Card(i, true);
+                deck.add(card);
+            }
+        }
+        return deck;
+    }
+
+    // 카드 뽑기
+    @Override
+    public List<String> draw() {
+        List<Card> deck = makeCard();
+        List<String> drawCardList = new ArrayList<>();
+        String drawDeck = "";
+        for (int i = 0; i < 4; i++) {
+            int index = (int) (Math.random() * 9);
+            deck.get(index);
+
+            int uniqueCk = (int) (Math.random() * 2);
+
+            if (uniqueCk == 0) {
+                drawDeck = String.valueOf(makeCard().get(index).getMonth());
+            } else {
+                drawDeck = makeCard().get(index).getMonth() + "U";
+            }
+
+            drawCardList.add(drawDeck);
+
+            for (int j = 0; j < i; j++) {
+                if (drawCardList.get(i).equals(drawCardList.get(j))) {
+                    drawCardList.remove(j);
+                    i--;
+                }
+            }
+        }
+
+        return drawCard = drawCardList;
+    }
